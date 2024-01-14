163a35d7d1900dfbde7fcb5ef95cbef6ae956a4b
Nguyễn Vĩnh Phúc <phucnv@clik.vn>
2024-01-14 23:11:29 +0700

    add move to top button

--- src/pages/ImageSlider/index.jsx
+++ src/pages/ImageSlider/index.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const images = [
   process.env.PUBLIC_URL + "/images/kiss.jpg",
-  process.env.PUBLIC_URL + "/images/kiss.jpg",
-  process.env.PUBLIC_URL + "/images/kiss.jpg",
+  process.env.PUBLIC_URL + "/images/kiss-1.jpg",
+  process.env.PUBLIC_URL + "/images/kiss-2.jpg",
 ];
 
 const ImageSlider = () => {
   return (
-    <div className="image-slider">
+    <div className="image-slider" style={{ backgroundColor: "white" }}>
       <Carousel infiniteLoop={true} interval={2000} showThumbs={false} autoPlay>
         {images.map((image) => (
           <div>
-            <img src={image} alt="" />
+            <LazyLoadImage src={image} alt="" effect="blur" width="100%" />
           </div>
         ))}
       </Carousel>
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { ParallaxProvider, Parallax } from "react-scroll-parallax";
+import ScrollToTop from "react-scroll-to-top";
+import ImageSlider from "../ImageSlider";
 import WelcomePage from "../WelcomePage";
 import StoryPage from "../StoryPage";
 import WeddingInfoDetailPage from "../WeddingInfoDetailPage";
 import MapPage from "../MapPage";
 import ThankYouPage from "../ThankYouPage";
+import ArrowTopIcon from "../../components/icon/ArrowTopIcon";
 
 import "./styles.scss";
-import ImageSlider from "../ImageSlider";
 
 const MainPage = () => {
   return (
@@ -17,6 +19,12 @@ const MainPage = () => {
           <div className="parallax-image"></div>
         </Parallax>
         <div className="content-wrapper">
+          <ScrollToTop
+            smooth
+            color="#6f00ff"
+            component={<ArrowTopIcon />}
+            className="button-move-to-top"
+          />
           <WelcomePage />
           <StoryPage />
           <ImageSlider />
