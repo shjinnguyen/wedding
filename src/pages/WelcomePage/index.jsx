df28c3c328180a6996dab0f1162116c74d0680ed
Nguyễn Vĩnh Phúc <phucnv@clik.vn>
2024-01-14 13:20:13 +0700

    add basic menu for mobile

--- src/pages/WelcomePage/index.jsx
+++ src/pages/WelcomePage/index.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import "./styles.scss";
 
 const WelcomePage = () => {
+  const [showMenu, setShowMenu] = React.useState(false);
+
   return (
-    <div className="welcome-page parallax">
+    <div className="welcome-page">
       <div className="content">
-        <div className="header">
+        <div className={`header ${showMenu ? "show-menu" : "hidden-menu"}`}>
           <ul className="menu-list">
             <li>
               <a href="google.com">TRANG CHỦ</a>
@@ -30,7 +32,15 @@ const WelcomePage = () => {
               <a href="google.com">TRẢ LỜI</a>
             </li>
           </ul>
+          <button className="button-hide" onClick={() => setShowMenu(false)}>
+            X
+          </button>
         </div>
+        {!showMenu && (
+          <button className="button-show" onClick={() => setShowMenu(true)}>
+            Show Menu
+          </button>
+        )}
         <div className="information-section">
           <h1>Vĩnh Phúc & Việt Trinh</h1>
           <p id="time-and-place">11:00 09/03/2024 | My Tho Marina</p>
